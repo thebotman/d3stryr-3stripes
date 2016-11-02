d2451f19acd3b469fe6a5fc006a1ced8afc194c8
Sasan Bahadaran <sasan.baha@gmail.com>
2016-11-01 23:53:52 -0400

    updated script with formatting

--- d3stryr-3stripes.py
+++ d3stryr-3stripes.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python3
-#Import from site-packages
+
+# Import from site-packages
 import json
-#Imports from destroyer.py
-from destroyer import d_,x_,lr_
-from destroyer import printRunParameters
-from destroyer import getProductInfo
-from destroyer import printProductInfo
-from destroyer import processAddToCart
+# Imports from destroyer.py
+from destroyer import d_, x_, lr_
+from destroyer import printRunParameters, getProductInfo, printProductInfo, \
+processAddToCart
+
 
 if __name__ == "__main__":
-  #Print the run parameters
-  printRunParameters()
-  #Get product info
-  productInfo=getProductInfo()
-  #Print product info
-  printProductInfo(productInfo)
-  #If product count is not zero process add to cart
-  if productInfo["productCount"] > 0:
-    processAddToCart(productInfo)
-  elif productInfo["productCount"] == -1:
-    print (d_()+x_("Variant Count")+lr_("-1"))
-    processAddToCart(productInfo)
-  else:
-    print (d_()+x_("Variant Count")+lr_("0"))
+    # Print the run parameters
+    printRunParameters()
+    # Get product info
+    productInfo = getProductInfo()
+    # Print product info
+    printProductInfo(productInfo)
+    # If product count is not zero process add to cart
+    if productInfo["productCount"] > 0:
+        processAddToCart(productInfo)
+    elif productInfo["productCount"] == -1:
+        print(d_() + x_("Variant Count") + lr_("-1"))
+        processAddToCart(productInfo)
+    else:
+        print(d_() + x_("Variant Count") + lr_("0"))
